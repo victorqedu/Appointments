e40ca91374f2cee2055b8ac9a62cad04d4a2b03d
victorqedu <victor@caido.ro>
2023-07-11 11:55:47 +0300

    Update AppointmentsApplication.java

--- src/main/java/com/caido/appointments/AppointmentsApplication.java
+++ src/main/java/com/caido/appointments/AppointmentsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-//@EntityScan({"com.caido.appointments.entity.*"})
+@EntityScan({"com.caido.appointments.entity"})
 public class AppointmentsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppointmentsApplication.class, args);
